1dc8f949924d40eb2992a6d52fb1712bf45fdecf
Sangey <Sangeylama@gmail.com>
2018-02-13 23:24:11 +0100

    pushing tiled resources

---
+++ TiledAssests/objects.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="348" tileheight="364" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="7">
+  <image width="348" height="364" source="../Platform_Phaser_Game/assets/images/ghost.png"/>
+ </tile>
+ <tile id="8">
+  <image width="270" height="258" source="../Platform_Phaser_Game/assets/images/spike A.png"/>
+ </tile>
+ <tile id="9">
+  <image width="184" height="195" source="../Platform_Phaser_Game/assets/images/spike B.png"/>
+ </tile>
+ <tile id="10">
+  <image width="149" height="299" source="../Platform_Phaser_Game/assets/images/spike C.png"/>
+ </tile>
+ <tile id="11">
+  <image width="159" height="326" source="../Platform_Phaser_Game/assets/images/spike D.png"/>
+ </tile>
+ <tile id="12">
+  <image width="24" height="22" source="../Platform_Phaser_Game/assets/images/star.png"/>
+ </tile>
+</tileset>
